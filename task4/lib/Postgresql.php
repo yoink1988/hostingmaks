c492063b9fb052a4929a51c5b3d3bc0d2da5bf58
yoink1988 <30431869+yoink1988@users.noreply.github.com>
2017-07-27 14:51:12 +0300

    first

---
+++ task4/lib/Postgresql.php
@@ -0,0 +1,86 @@
+<?php
+
+
+class Postgresql extends Db
+{
+
+	private function getConnect()
+	{
+
+	if($this->link !=null)
+	{
+		return $this->link;
+	}
+	if($this->link = pg_connect("host=localhost, dbname=, user=, password=" ))
+	{
+		return $this->link;
+	}
+//		if($this->link != null)
+//		{
+//			return $this->link;
+//		}
+//		if($this->link = mysql_connect('localhost', 'root', ''))
+//		{
+//			mysql_select_db('test');
+//			return $this->link;
+//		}
+	}
+
+
+
+	public function exec()
+	{
+		parent::exec();
+
+		switch ($this->queryType)
+		{
+			case 'insert':
+				if(!pg_query($this->query, $this->getConnect()))
+				{
+					return false;
+				}
+				return SUCCESS;
+
+			case 'update':
+				if(!pg_query($this->query, $this->getConnect()))
+				{
+					return false;
+				}
+				return SUCCESS;
+			case 'select':
+				$result=  array();
+				if(!$stmt = pg_query($this->query, $this->getConnect()))
+				{
+					return false;
+				}
+				while($res = pg_fetch_array($stmt, MYSQL_ASSOC))
+				{
+					$result[]=$res;
+				}
+				return $result;
+
+			case 'delete':
+				echo $this->query;
+				if(pg_query($this->query, $this->getConnect()))
+				{
+					return pg_affected_rows($this->getConnect()). " row deleted";
+				}
+				else
+				{
+					return false;
+				}
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+	public function __construct(){}
+}
---
+++ task4/lib/config.php
@@ -0,0 +1,23 @@
+<?php
+
+define ("ROOT" , __DIR__ . "/..");
+
+
+define ("MYSQL_HOST", "localhot");
+define ("MYSQL_DB_NAME", "test");
+define ("MYSQL_USER", "user1");
+define ("MYSQL_PASS", "tuser1");
+define ("MYSQL_TABLE_NAME", "MY_TEST");
+
+define ("POSTGRE_HOST", "localhot");
+define ("POSTGRE_DB_NAME", "test");
+define ("POSTGRE_USER", "user1");
+define ("POSTGRE_PASS", "tuser1");
+define ("POSTGRE_TABLE_NAME", "MY_TEST");
+
+define ("DB_MYSQL" , 1);
+define ("DB_POSTGRE" , 2);
+
+define ("DB_DRIVER" , 1);
+
+define ("SUCCESS" , "SUCCESS");
